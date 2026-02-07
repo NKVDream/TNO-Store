dc07ab8a3be5c7f41db6c11040d08d1b0c5b8fee
Владислав Капустин <kmv235@yandex.ru>
2026-02-07 14:58:57 +0500

    Contrs in prog

--- TNOGS/TNOGS/Controllers/ProductController.cs
+++ TNOGS/TNOGS/Controllers/ProductController.cs
@@ -19,5 +19,26 @@ namespace TNOGS.Controllers
         {
             return Ok(_context.Products.ToList());
         }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)//async С async: Поток "освобождается" на время ожидания.
+        {
+            // await говорит: "подожди здесь, пока база ответит, но не блокируй поток"
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync(); // Сохраняем изменения асинхронно
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
+
     }
 }
